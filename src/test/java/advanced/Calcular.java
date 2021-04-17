f36ce21567d346275568e44ec90637d9bf4c676c
MarisaMendes-qa <marisamendes.qa@gmail.com>
2021-04-16 23:03:30 -0300

    Atualização da classe Calcular.java com acréscimos do Método print gerando prints dos testes na pasta target com Data e Hora.

--- src/test/java/advanced/Calcular.java
+++ src/test/java/advanced/Calcular.java
@@ -7,23 +7,37 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import static org.junit.Assert.assertEquals;
 
-public class Calcular {
+    public class Calcular {
 
-    private AndroidDriver driver;
-    private DesiredCapabilities desiredCapabilities;
-    private URL remoteUrl;
-
-    public Calcular() {
-    }
+        private AndroidDriver driver;
+        private DesiredCapabilities desiredCapabilities;
+        private URL remoteUrl;
+        private static String nomePasta = new SimpleDateFormat("yyyy-MM-dd HH-mm").format(Calendar.getInstance().getTime());
 
 
-    @Before
-    public void setUp() throws MalformedURLException {
+        // Funções ou Métodos de Apoio
+        public void print(String nomePrint) throws IOException {
+            File foto = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(foto, new File("target/" + nomePasta + "/" + nomePrint + ".png"));
+
+        }
+
+
+        @Before
+        public void setUp() throws MalformedURLException {
          /*
         Valores previstos de flag
         EL = Emulador Local
@@ -31,93 +45,98 @@ public class Calcular {
         DL = Dispositivo Local
         DN = Dispositivo na Nuvem
          */
-        String flag = "EN";
+            String flag = "EN";
 
-        desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities = new DesiredCapabilities();
 
-        switch (flag){
-            case "EL":
-                desiredCapabilities.setCapability("platformName", "Android");
-                desiredCapabilities.setCapability("platformVersion", "9.0"); //sdk
-                desiredCapabilities.setCapability("deviceName", "emulator-5554");
-                desiredCapabilities.setCapability("automationName", "uiautomator2");
-                desiredCapabilities.setCapability("appPackage", "com.google.android.calculator");
-                desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+            switch (flag) {
+                case "EL":
+                    desiredCapabilities.setCapability("platformName", "Android");
+                    desiredCapabilities.setCapability("platformVersion", "9.0"); //sdk
+                    desiredCapabilities.setCapability("deviceName", "emulator-5554");
+                    desiredCapabilities.setCapability("automationName", "uiautomator2");
+                    desiredCapabilities.setCapability("appPackage", "com.google.android.calculator");
+                    desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
 
-                remoteUrl = new URL("https://127.0.0.1:4723/wd/hub");
-                break;
+                    remoteUrl = new URL("https://127.0.0.1:4723/wd/hub");
+                    break;
 
-            case "EN":
-                desiredCapabilities.setCapability("platformName", "Android");
-                desiredCapabilities.setCapability("platformVersion", "9.0");
-                desiredCapabilities.setCapability("browserName", "");
-                desiredCapabilities.setCapability("appiumVersion", "1.19.2");
-                desiredCapabilities.setCapability("deviceName", "Samsung Galaxy S9 FHD GoogleAPI Emulator");
-                desiredCapabilities.setCapability("deviceOrientation", "portrait");
-                desiredCapabilities.setCapability("app", "storage:filename=Calculator_v7.8 (271241277)_apkpure.com.apk");
-                desiredCapabilities.setCapability("appPackage", "com.google.android.calculator");
-                desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
-                desiredCapabilities.setCapability("ensureWebviewsHavePages", true);
-                desiredCapabilities.setCapability("SAUCE_USERNAME", "marisamendes");
-                desiredCapabilities.setCapability("SAUCE_ACCESS_KEY", "1e4b16f2-e25d-426f-98f9-1a7b6cbf7a83");
+                case "EN":
+                    desiredCapabilities.setCapability("platformName", "Android");
+                    desiredCapabilities.setCapability("platformVersion", "9.0");
+                    desiredCapabilities.setCapability("browserName", "");
+                    desiredCapabilities.setCapability("appiumVersion", "1.19.2");
+                    desiredCapabilities.setCapability("deviceName", "Samsung Galaxy S9 FHD GoogleAPI Emulator");
+                    desiredCapabilities.setCapability("deviceOrientation", "portrait");
+                    desiredCapabilities.setCapability("app", "storage:filename=Calculator_v7.8 (271241277)_apkpure.com.apk");
+                    desiredCapabilities.setCapability("appPackage", "com.google.android.calculator");
+                    desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+                    desiredCapabilities.setCapability("ensureWebviewsHavePages", true);
+                    desiredCapabilities.setCapability("SAUCE_USERNAME", "marisamendes");
+                    desiredCapabilities.setCapability("SAUCE_ACCESS_KEY", "1e4b16f2-e25d-426f-98f9-1a7b6cbf7a83");
 
-                URL remoteUrl = new URL("https://marisamendes:1e4b16f2-e25d-426f-98f9-1a7b6cbf7a83@ondemand.us-west-1.saucelabs.com:443/wd/hub");
-                break;
+                    URL remoteUrl = new URL("https://marisamendes:1e4b16f2-e25d-426f-98f9-1a7b6cbf7a83@ondemand.us-west-1.saucelabs.com:443/wd/hub");
+                    break;
 
-            case "DL":
-                //TODO: a programar
-                break;
+                case "DL":
+                    //TODO: a programar
+                    break;
 
-            case "DN":
-                //TODO: a programar
-                break;
+                case "DN":
+                    //TODO: a programar
+                    break;
+            }
+
+            desiredCapabilities.setCapability("platformName", "Android");
+            desiredCapabilities.setCapability("platformVersion", "9.0");
+            desiredCapabilities.setCapability("browserName", "");
+            desiredCapabilities.setCapability("appiumVersion", "1.19.2");
+            desiredCapabilities.setCapability("deviceName", "Samsung Galaxy S9 FHD GoogleAPI Emulator");
+            desiredCapabilities.setCapability("deviceOrientation", "portrait");
+            desiredCapabilities.setCapability("app", "storage:filename=Calculator_v7.8 (271241277)_apkpure.com.apk");
+            desiredCapabilities.setCapability("appPackage", "com.google.android.calculator");
+            desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+            desiredCapabilities.setCapability("ensureWebviewsHavePages", true);
+
+            remoteUrl = new URL("https://marisamendes:1e4b16f2-e25d-426f-98f9-1a7b6cbf7a83@ondemand.us-west-1.saucelabs.com:443/wd/hub");
+
+
         }
 
-        desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("platformVersion", "9.0");
-        desiredCapabilities.setCapability("browserName", "");
-        desiredCapabilities.setCapability("appiumVersion", "1.19.2");
-        desiredCapabilities.setCapability("deviceName", "Samsung Galaxy S9 FHD GoogleAPI Emulator");
-        desiredCapabilities.setCapability("deviceOrientation", "portrait");
-        desiredCapabilities.setCapability("app", "storage:filename=Calculator_v7.8 (271241277)_apkpure.com.apk");
-        desiredCapabilities.setCapability("appPackage", "com.google.android.calculator");
-        desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
-        desiredCapabilities.setCapability("ensureWebviewsHavePages", true);
+        @After
+        public void tearDown() {
 
-        remoteUrl = new URL("https://marisamendes:1e4b16f2-e25d-426f-98f9-1a7b6cbf7a83@ondemand.us-west-1.saucelabs.com:443/wd/hub");
+            driver.quit();
+        }
 
 
+        @Given("^abro a calculadora do Google no meu smartPhone$")
+        public void abroACalculadoraDoGoogleNoMeuSmartPhone() {
+            driver = new AndroidDriver(remoteUrl, desiredCapabilities); //Essas duas linhas abrem o aparelho
+        }
+
+        @When("^seleciono \"([^\"]*)\" mais \"([^\"]*)\" e pressiono o botao Igual$")
+        public void selecionoMaisEPressionoOBotaoIgual(String num1, String num2) throws IOException {
+            print("Somar Dois Numeros Positivos - Passo 1 - Abriu a Calculadora");
+            MobileElement btnA = (MobileElement) driver.findElementById("com.google.android.calculator:id/digit_" + num1);
+            btnA.click();
+            print("Somar Dois Numeros Positivos - Passo 2 - Clicou no botão " + num1);
+            MobileElement btnSoma = (MobileElement) driver.findElementByAccessibilityId("plus");
+            btnSoma.click();
+            print("Somar Dois Numeros Positivos - Passo 3 - Clicou no botão de Soma");
+            MobileElement btnB = (MobileElement) driver.findElementById("com.google.android.calculator:id/digit_" + num2);
+            btnB.click();
+            print("Somar Dois Numeros Positivos - Passo 4 - Clicou no botão " + num2);
+            MobileElement btnIgual = (MobileElement) driver.findElementByAccessibilityId("equals");
+            btnIgual.click();
+            print("Somar Dois Numeros Positivos - Passo 5 - Clicou no botão Igual");
+        }
+
+        @Then("^exibe o resultado como \"([^\"]*)\"$")
+        public void exibeOResultadoComo(String resultadoEsperado) {
+            MobileElement lblResultadoAtual = (MobileElement) driver.findElementById("com.google.android.calculator:id/result_final");
+            assertEquals(resultadoEsperado, lblResultadoAtual.getText());
+
+        }
     }
-
-    @After
-    public void tearDown() {
-        driver.quit();
-    }
-
-
-    @Given("^abro a calculadora do Google no meu smartPhone$")
-    public void abroACalculadoraDoGoogleNoMeuSmartPhone() {
-        driver = new AndroidDriver(remoteUrl, desiredCapabilities); //Essas duas linhas abrem o aparelho
-    }
-
-    @When("^seleciono \"([^\"]*)\" mais \"([^\"]*)\" e pressiono o botao Igual$")
-    public void selecionoMaisEPressionoOBotaoIgual(String num1, String num2)  {
-        MobileElement btnA = (MobileElement) driver.findElementById("com.google.android.calculator:id/digit_" + num1);
-        btnA.click();
-        MobileElement btnSoma = (MobileElement) driver.findElementByAccessibilityId("plus");
-        btnSoma.click();
-        MobileElement btnB = (MobileElement) driver.findElementById("com.google.android.calculator:id/digit_" + num2);
-        btnB.click();
-        MobileElement btnIgual = (MobileElement) driver.findElementByAccessibilityId("equals");
-        btnIgual.click();
-
-    }
-
-    @Then("^exibe o resultado como \"([^\"]*)\"$")
-    public void exibeOResultadoComo(String resultadoEsperado)  {
-        MobileElement lblResultadoAtual = (MobileElement) driver.findElementById("com.google.android.calculator:id/result_final");
-        assertEquals(resultadoEsperado, lblResultadoAtual.getText());
-
-    }
-}
 
--- src/test/resources/features/Calcular.feature
+++ src/test/resources/features/Calcular.feature
@@ -1,5 +1,17 @@
 Feature: Calcular
   Scenario: Somar Dois Números Inteiros Positivos
     Given abro a calculadora do Google no meu smartPhone
-    When seleciono "2" mais "3" e pressiono o botao Igual
-    Then exibe o resultado como "5"
+    When seleciono "8" mais "5" e pressiono o botao Igual
+    Then exibe o resultado como "13"
+
+    Scenario Outline: Somar Dois Numero Data
+      Given abro a calculadora do Google no meu smartPhone
+      When seleciono <num1> mais <num2> e pressiono o botao Igual
+      Then exibe o resultado como <resultadoEsperado>
+      Examples:
+
+      | num1 | num2| resultadoEsperado |
+      | "8"  | "5" | "13"              |
+      | "7"  | "9" | "16"              |
+      | "8"  | "5" | "6"               |
+
